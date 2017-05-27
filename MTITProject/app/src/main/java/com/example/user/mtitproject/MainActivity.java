1d776039e2cecfb351a8a72f5875e897ff9d162e
Althaff <althaff1994@gmail.com>
2017-05-27 17:19:15 +0530

    Flash switch button

--- MTITProject/app/src/main/java/com/example/user/mtitproject/MainActivity.java
+++ MTITProject/app/src/main/java/com/example/user/mtitproject/MainActivity.java
@@ -22,5 +22,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // flash switch button
+        btnSwitch = (ImageButton) findViewById(R.id.imageButton);
+
+
+        // First check if device is supporting flashlight or not
+        hasFlash = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!hasFlash) {
+            // device doesn't support flash
+            // Show alert message and close the application
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                    .create();
+            alert.setTitle("Error");
+            alert.setMessage("Your device doesn't support flash light!");
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                }
+            });
+            alert.show();
+            return;
+        }
     }
 }
